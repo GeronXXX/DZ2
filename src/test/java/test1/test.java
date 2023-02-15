c096d3644694ce47c5dd4058836cb64acf4a1450
Ivan <lityagin.ru@gmail.com>
2023-02-16 00:22:46 +0300

    conflict_4

--- src/test/java/test1/test.java
+++ src/test/java/test1/test.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class test {
     @Test
     void shouldOpen() {
-        String date = "12.02.2026";
+        String date = "12.02.2027";
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
